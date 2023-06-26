2548e6ea786ed24e63216fd10c2b8384578ff345
ULISES ANIBAL BAAMONDE <45687573@ort>
2023-06-26 15:59:47 -0300

    Tratando de que se muestre la bandera

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import './App.css';
 import Juego from './Juego';
 
 function App() {
+  const [pregunta, setPregunta] = useState('');
+
+    const agregarBandera = (contenido) => {
+      const nuevaBandera = {
+        nombre: contenido.name,
+        imagen: contenido.flag,
+      };
+      setPregunta([...pregunta, nuevaBandera])
+    };
+
   return (
-    <Juego />
+    <Juego onNuevaBandera={agregarBandera} />
   );
 }
 
--- src/Juego.js
+++ src/Juego.js
@@ -2,31 +2,41 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Juego() {
+function Juego({onNuevaBandera}) {
     const [isLoading, setIsLoading] = useState(true);
     const [pais, setPais] = useState('');
-    const [pregunta, setPregunta] = useState('');
- //   const [respuesta, setRespuesta] = useState('');
+    //const [respuesta, setRespuesta] = useState('');
 
     useEffect(() => {
         axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
             .then((res) => {
-            setPais(res.data.data);
-            setIsLoading(false);
-            })
+                            //console.log(res)
+                            setPais(res.data.data);
+                            setIsLoading(false);
+                            })
             .catch((error) => {
-            console.log(error);
-            setIsLoading(false);
-            });
-        }, []);
+                                 console.log(error);
+                                setIsLoading(false);
+                                });
+       
+   }, []);
 
-    function ConseguirBandera() {
+    console.log(pais)
+   const ConseguirBandera = () => {
         console.log(pais)
         let posicion = Math.floor(Math.random() * 220)
-        setPregunta(pais[posicion])
-        console.log(pregunta.flag)
-        return pregunta.flag
+        const nuevaBandera ={
+           nombre: pais[posicion].name,
+           imagen: pais[posicion].flag,
+        }
+        console.log(nuevaBandera)
+        onNuevaBandera(nuevaBandera)
+        //console.log(pregunta.flag)
+        //setRespuesta(pregunta.name)
     }
+
+
+
 
     if(isLoading){
     return(
@@ -39,8 +49,12 @@ function Juego() {
     return(
         <div className='App'>
             <h3> ¿Qué bandera es esta? </h3>
-            <img src={ConseguirBandera()} alt='Una bandera'/>
+            <div onLoad={ConseguirBandera}/>
+            <img></img>
+            <form>
             <input type="text" id="bandera" name="bandera" required> Adivine el pais: </input>
+            <input type="submit" value='Submit'> Enviar </input>
+            </form>
         </div>
     );
     
